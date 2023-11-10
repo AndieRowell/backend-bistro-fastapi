e86b297eff4c6f2103a5a0a8e1736725e53b6877
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-11-10 02:44:26 +0000

    got tables created

---
+++ database.py
@@ -0,0 +1,9 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/postgres" #check for path in sql tools
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
---
+++ models.py
@@ -0,0 +1,6 @@
+from typing import List
+from typing import Optional
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Text, Table
+
