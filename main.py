c4813fbe9e663a714972c558efa920143c544c58
Andie <134252065+AndieRowell@users.noreply.github.com>
2023-11-10 05:12:51 +0000

    adding relationship

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 from fastapi import FastAPI #import fastapi
 from typing import List
+#need to import models created from models.py file
+
+#need to import sqlalchemy
+
+#need to import routes? - need clarification on routes and router
+
+
 
 app = FastAPI() #creates an api object - initializes it - always need to have this
+#previous project also had "app.include_router(heroes.router)"
+    #figure out what this does exactly - how does this connect?
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get
--- models.py
+++ models.py
@@ -18,12 +18,21 @@ class Menu_item(Base):
     price: Mapped[int] = Column(Integer, default="Price")
     spicy_level: Mapped[int] = Column(Integer, default="Spiciness Level")
 
-    #need to put the connection/relationship to the othe tables
-
-
+    #need to put the connection/relationship to the other tables
+        #does this need to have cuisine id mapped to the other table here instead of above?
+        # - does it matter if out of order compared to sql
+        # - am i still using a list?
+    cuisine_id: Mapped[List["Cuisine"]] = relationship(back_populates="menu_item", cascade="all, delete-orphan")
 
     #also define the function to return the information using string interpolation
+    #this code below autofilled in...
+        # def __repr__(self) -> str:
+        #     return super().__repr__()
+    def __repr__(self) -> str:
+        return f"Menu Item(id={self.id!r}, title={self.title!r}, cuisine={self.cuisine_id!r}, category={self.category_id!r}, description={self.description!r}, price={self.price!r}, spicy_level={self.spicy_level!r})"
 
 # create another class for the cuisine table
+class Cuisine(Base):
+
 
 # create another class for the category table
